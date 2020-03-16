919207793d15146851ffe681378f303d4e419bd7
unknown <gaffney339911@gmail.com>
2020-03-16 13:05:11 -0500

    https fix removal

--- src/gamesearch/GameSearch.jsx
+++ src/gamesearch/GameSearch.jsx
@@ -14,7 +14,7 @@ const GameSearch = (props) => {
 
     //fetchSearch grabs the game to be placed in the card
     const fetchSearch = () => {
-        fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games', {
+        fetch('https://api-v3.igdb.com/games', {
             method: 'POST',
             body: `search "${searchTitle}"; fields name, first_release_date, cover, summary, age_ratings;`,
             headers: new Headers({
@@ -32,7 +32,7 @@ const GameSearch = (props) => {
 
     //fetchCover grabs the box art url to be displayed in the card
     const fetchCover = (id) => {
-        fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers', {
+        fetch('https://api-v3.igdb.com/covers', {
             method: 'POST',
             body: `where id = ${id}; fields url;`,
             headers: new Headers({
@@ -47,7 +47,7 @@ const GameSearch = (props) => {
 
     //fetchRatings grabs the rating integer
     const fetchRatings = (ratingId) => {
-        fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/age_ratings', {
+        fetch('https://api-v3.igdb.com/age_ratings', {
             method: 'POST',
             body: `where id = ${ratingId}; fields rating;`,
             headers: new Headers({
