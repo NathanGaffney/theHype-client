16486c12220cba2de0bd3b6137ac0a241860dbe6
unknown <gaffney339911@gmail.com>
2020-03-17 16:17:41 -0500

    fetch rework

--- src/gamesearch/GameSearchDisplay.jsx
+++ src/gamesearch/GameSearchDisplay.jsx
@@ -7,14 +7,18 @@ import TimeConversion from './TimeConversion';
 // import CoverGrab from './CoverGrab';
 import './GameSearchDisplay.css';
 import APIURL from '../helpers/environment';
+import { timeConverter } from './TimeConversion';
 
 
 const GameSearchDisplay = (props) => {
-    const [title, setTitle] = useState(props.searchGame[0].name);
+    // const [title, setTitle] = useState(props.searchGame[0].name);
     const [hypeRating, setHypeRating] = useState(0);
-    const [description, setDescription] = useState(props.searchGame[0].summary);
-    const [releaseDate, setReleaseDate] = useState(props.searchGame[0].first_release_date);
-    const [platform, setPlatform] = useState('choose');
+    const [description, setDescription] = useState(props.searchGame.summary);
+    const [releaseDate, setReleaseDate] = useState(props.searchGame.first_release_date);
+    const [coverUrl, setCoverUrl] = useState('');
+    const [ratingUrl, setRatingUrl] = useState('');
+    const [searchPlatforms, setSearchPlatforms] = useState([])
+    // const [platform, setPlatform] = useState('choose');
     
     const handleSubmit = () => {
 
@@ -22,11 +26,11 @@ const GameSearchDisplay = (props) => {
         fetch(`${APIURL}/game/create`, {
             method: 'POST',
             body: JSON.stringify({ 
-                title: title, 
+                title: props.searchGame.name, 
                 hypeRating: hypeRating, 
-                description: description, 
-                releaseDate: releaseDate, 
-                platform: platform }),
+                description: props.searchGame.summary, 
+                releaseDate: timeConverter(releaseDate), 
+                platform: 'choose' }),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': props.token
@@ -34,59 +38,149 @@ const GameSearchDisplay = (props) => {
         }).then((res) => res.json())
             .then((gameData) => {
                 console.log('THIS RAN', gameData);
-                props.setSearchGame([])
+                // props.setSearchGame([])
                 alert('Game Added')
             }).catch(err => console.log(err))
     }
 
-    // const [releaseDateId, setReleaseDateId] = useState('');
-    const [coverId, setCoverId] = useState([]);
+    //fetchCover grabs the box art url to be displayed in the card
+    const fetchCover = (id) => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers', {
+            method: 'POST',
+            body: `where id = ${id}; fields url;`,
+            headers: new Headers({
+                'user-key': process.env.REACT_APP_IGDB_API_KEY,
+                'Content-Type': 'application/json'
+            })
+        }).then((res) => res.json())
+            .then((coverData) => {
+                setCoverUrl(coverData[0].url);
+            }).catch(err => console.log(err))
+    }
 
+    
+
+    //fetchRatings grabs the rating integer
+    const fetchRatings = (ratingId) => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/age_ratings', {
+            method: 'POST',
+            body: `where id = ${ratingId}; fields rating;`,
+            headers: new Headers({
+                'user-key': process.env.REACT_APP_IGDB_API_KEY,
+                'Content-Type': 'application/json'
+            })
+        }).then((res) => res.json())
+            .then((ratingData) => {
+                console.log('RATING DATA', ratingData)
+                setRatingUrl(ratingData[0].rating);
+            }).catch(err => console.log(err))
+    }
+
+
+
+
+
+
+
+
+
+    //this isnt complete---------------------------------------------------------------
+    // const fetchPlatforms = (id) => {
+    //     fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/platforms', {
+    //         method: 'POST',
+    //         body: `where id = ${id}; fields abbreviation;`,
+    //         headers: new Headers({
+    //             'user-key': process.env.REACT_APP_IGDB_API_KEY,
+    //             'Content-Type': 'application/json'
+    //         })
+    //     }).then((res) => res.json())
+    //     .then((platformData) => {
+    //         console.log('PLATFORM FETCH RAN', platformData)
+    //         setSearchPlatforms(platformData)
+    //     })
+    // }
+
+    // let plats = null;
+    // if(props.searchGame.platforms > 0){
+    //     plats = props.searchGame.platforms.map((item, index)=>{
+    //         return fetchPlatforms(item)
+    //     });
+    // }
+    //--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    
     const checkProps = () => {
         console.log('SEARCH PROPS', props)
     }
 
-    useEffect(() => { setCoverId(props.searchGame[0].cover) })
-
-    //this sets the rating
+    //this sets the rating 
     const handleRating = () => {
-        if (props.ratingUrl === 6) {
+        if (ratingUrl === 6) {
             return (<p>ESRB Rating: RP (Rating Pending)</p>)
-        } else if (props.ratingUrl === 7) {
+        } else if (ratingUrl === 7) {
             return (<p>ESRB Rating: EC (Early Childhood)</p>)
-        } else if (props.ratingUrl === 8) {
+        } else if (ratingUrl === 8) {
             return (<p>ESRB Rating: E (Everyone)</p>)
-        } else if (props.ratingUrl === 9) {
+        } else if (ratingUrl === 9) {
             return (<p>ESRB Rating: E10 (Everyone 10+)</p>)
-        } else if (props.ratingUrl === 10) {
+        } else if (ratingUrl === 10) {
             return (<p>ESRB Rating: T (Teen)</p>)
-        } else if (props.ratingUrl === 11) {
+        } else if (ratingUrl === 11) {
             return (<p>ESRB Rating: M (Mature)</p>)
-        } else if (props.ratingUrl === 12) {
+        } else if (ratingUrl === 12) {
             return (<p>ESRB Rating: AO (Adult Only)</p>)
         } else {
             return ('')
         }
     }
 
+    useEffect(() => {
+        if(props.searchGame.cover){
+            fetchCover(props.searchGame.cover)
+        }
+        if(props.searchGame.age_ratings){
+            fetchRatings(props.searchGame.age_ratings[0])
+        }
+
+        //this isnt complete-----------------------------------------------------------
+        // if(plats){
+        //     fetchPlatforms(plats)
+        // }
+        //-------------------------------------------------------------------------------
+        
+    }, [])
+    
     return (
         <>
         {checkProps()}
             <hr />
-            <Row>
                 <Col sm='4'>
                     <Card>
-                        <CardImg top width='100%' src={props.coverUrl} alt='Card image cap' />
+                        <CardImg top width='100%' src={coverUrl} alt='Card image cap' />
                         <CardBody>
-                            <CardTitle>{props.searchGame[0].name}</CardTitle>
-                            <CardSubtitle><TimeConversion searchGame={props.searchGame} /></CardSubtitle>
+                            <CardTitle>{props.searchGame.name}</CardTitle>
+                            <CardSubtitle>
+                                {props.searchGame.first_release_date ? 
+                                    timeConverter(props.searchGame.first_release_date) :
+                                    ''}
+                            </CardSubtitle>
                             <CardSubtitle>{handleRating()}</CardSubtitle>
-                            <CardText className='description'>{props.searchGame[0].summary}</CardText>
+
+                            {/* <CardSubtitle>{plats}</CardSubtitle> */}
+
+                            <CardText className='description'>{props.searchGame.summary ? props.searchGame.summary : ''}</CardText>
                         </CardBody>
                         <Button color='success' onClick={() => handleSubmit()}>Add</Button>
                     </Card>
                 </Col>
-            </Row>
         </>
     )
 }
--- src/gamesearch/TimeConversion.jsx
+++ src/gamesearch/TimeConversion.jsx
@@ -1,27 +1,14 @@
-import React, {useState} from "react";
-
-const TimeConversion = (props) => {
-  const[rdtime, setRdtime] = useState('')
-  function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    // var hour = a.getHours();
-    // var min = a.getMinutes();
-    // var sec = a.getSeconds();
-    var time =
-    month + " " + date + " " + year // + " " + hour + ":" + min + ":" + sec;
-    console.log(a);
-    return time;
-  }
-  // console.log(timeConverter(props.searchGame[0].first_release_date));
-  return (
-    <div>
-      <p> Release Date: {timeConverter(props.searchGame[0].first_release_date)}</p>
-    </div>
-  );
+export function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  // var hour = a.getHours();
+  // var min = a.getMinutes();
+  // var sec = a.getSeconds();
+  var time =
+  month + " " + date + " " + year // + " " + hour + ":" + min + ":" + sec;
+  // console.log(a);
+  return time;
 }
-
-export default TimeConversion;
